7e14dece2003933a356cb2a137f931c63e4e4490
Hamayoun Sardar <hamayounawan127@gmail.com>
2022-05-11 11:29:51 +0500

    Array List

--- app/src/main/java/com/symplified/order/ProductsActivity.java
+++ app/src/main/java/com/symplified/order/ProductsActivity.java
@@ -10,20 +10,15 @@ import com.symplified.order.models.product.Products;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductsActivity extends AppCompatActivity {
-
+public class ProductsActivity extends AppCompatActivity
+{
     private RecyclerView productsrecyclerView;
     private ArrayList<Products> productsList;
     private static final String TAG = "ProductsActivity";
 
-//    public ProductsActivity() {}
-
-//    public ProductsActivity(int img, String name) {
-//    }
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceStatus) {
+    protected void onCreate(Bundle savedInstanceStatus)
+    {
         super.onCreate(savedInstanceStatus);
         setContentView(R.layout.activity_products);
 
@@ -38,7 +33,8 @@ public class ProductsActivity extends AppCompatActivity {
         productsrecyclerView = findViewById(R.id.productsRecyclerView);
     }
 
-    private void generateData() {
+    private void generateData()
+    {
         productsList.add(new Products(R.drawable.mob, "Mobile"));
         productsList.add(new Products(R.drawable.cam, "Camera"));
         productsList.add(new Products(R.drawable.leptop, "Leptop"));
@@ -52,48 +48,9 @@ public class ProductsActivity extends AppCompatActivity {
         productsList.add(new Products(R.drawable.mob, "Router"));
     }
 
-    private void setData() {
+    private void setData()
+    {
         productsrecyclerView.setLayoutManager(new LinearLayoutManager(this));
         productsrecyclerView.setAdapter(new ProductsListAdapter(this, productsList));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//        RecyclerView productsRecyclerView = findViewById(R.id.productsRecyclerView);
-//        productsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        String[] products = {"Leptop" , "Mobile" , "Mouse" , "Kayboard" , "Cell" , "Clock" , "Phone" , "Table" , "Chair" , "HeadPhones" , "Plate" , "Spoon" , "Board" , "Shoes" , "Glasses"};
-//        productsRecyclerView.setAdapter(new ProductsListAdapter(products));
-
-
-//    private void initData() {
-//        productsList = new ArrayList<>();
-//        productsList.add(new ProductsActivity(R.drawable.mob, "Mobile", "5"));
-//        productsList.add(new ProductsActivity(R.drawable.cam, "Camera", "7"));
-//        productsList.add(new ProductsActivity(R.drawable.leptop, "Leptop", "3"));
-//        productsList.add(new ProductsActivity(R.drawable.keyboard, "Keyboard", "2"));
-//        productsList.add(new ProductsActivity(R.drawable.mouse, "Mouse", "11"));
-//        productsList.add(new ProductsActivity(R.drawable.chair, "Chair", "20"));
-//        productsList.add(new ProductsActivity(R.drawable.table, "Table", "24"));
-//        productsList.add(new ProductsActivity(R.drawable.bike, "Bike", "14"));
-//        productsList.add(new ProductsActivity(R.drawable.tab, "Tablet", "55"));
-//        productsList.add(new ProductsActivity(R.drawable.lcd_81, "LCD", "12"));
-//        productsList.add(new ProductsActivity(R.drawable.mob, "Router", "1"));
-//    }
-//    private void initRecyclerView() {
-//        precyclerView = findViewById(R.id.productsRecyclerView);
-//        layoutManager = new LinearLayoutManager(this);
-//        layoutManager.setOrientation(RecyclerView.VERTICAL);
-//        precyclerView.setLayoutManager(layoutManager);
-//        precyclerView.setAdapter(productsListAdapter);
-//        productsListAdapter.notifyDataSetChanged();
-//    }
